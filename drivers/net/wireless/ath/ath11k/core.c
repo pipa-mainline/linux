c7b2b2282a0c963146b2ca42ec46060d600b290f
David Wronek <davidwronek@gmail.com>
2024-04-14 13:22:33 +0200

    wifi gets power, but ath11k driver failes to start

--- drivers/net/wireless/ath/ath11k/core.c
+++ drivers/net/wireless/ath/ath11k/core.c
@@ -4,6 +4,8 @@
  * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
+#define DEBUG
+
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/remoteproc.h>
