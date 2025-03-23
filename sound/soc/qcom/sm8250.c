a2723488a988b60c55b1de93b4e808fdf840a75c
domin746826 <ominek.pl91@gmail.com>
2025-03-23 09:59:18 +0000

    initial audio commit, aw88261-wip branch changes and rmux changes

--- sound/soc/qcom/sm8250.c
+++ sound/soc/qcom/sm8250.c
@@ -14,8 +14,11 @@
 #include "common.h"
 #include "sdw.h"
 
-#define DRIVER_NAME		"sm8250"
-#define MI2S_BCLK_RATE		1536000
+#define DRIVER_NAME "sm8250"
+#define MI2S_BCLK_RATE 1536000
+#define TDM_BCLK_RATE 12288000
+
+static unsigned int tdm_slot_offset[8] = { 0, 4, 8, 12, 16, 20, 24, 28 };
 
 struct sm8250_snd_data {
 	bool stream_prepared[AFE_PORT_MAX];
@@ -28,17 +31,103 @@ struct sm8250_snd_data {
 static int sm8250_snd_init(struct snd_soc_pcm_runtime *rtd)
 {
 	struct sm8250_snd_data *data = snd_soc_card_get_drvdata(rtd->card);
+	printk(KERN_INFO "sm8250_snd_init test1xd\n");
 
 	return qcom_snd_wcd_jack_setup(rtd, &data->jack, &data->jack_setup);
+}
+
+static int sm8250_tdm_snd_hw_params(struct snd_pcm_substream *substream,
+				    struct snd_pcm_hw_params *params)
+{
+	struct snd_soc_pcm_runtime *rtd = snd_soc_substream_to_rtd(substream);
+	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
+	struct snd_soc_card *card = rtd->card;
+	int ret = 0;
+	int channels, slots, slot_width;
+	bool is_aw88261 = false;
+	struct snd_soc_dai *codec_dai;
+	int j;
+
+	/* Check if we're dealing with an AW88261 codec */
+	for_each_rtd_codec_dais(rtd, j, codec_dai) {
+		if (strstr(codec_dai->component->name, "aw88261") != NULL) {
+			is_aw88261 = true;
+			break;
+		}
+	}
+
+	channels = params_channels(params);
+
+	/* Configure TDM with more slots for quad-speaker setup */
+	if (is_aw88261) {
+		slots = 8; /* 8 slots for quad-speaker setup */
+		slot_width = 32;
+	} else {
+		slots = 8;
+		slot_width = 32;
+	}
+
+	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
+		/* Special case for AW88261 (Xiaomi Pad 6) */
+		if (is_aw88261 && cpu_dai->id == TERTIARY_TDM_RX_0) {
+			dev_info(
+				card->dev,
+				"Applying AW88261 specific TDM settings (quad-speaker setup)\n");
+
+			/* AW88261 quad-speaker needs all 4 slots active */
+			ret = snd_soc_dai_set_tdm_slot(cpu_dai, 0, 0x0F, slots,
+						       slot_width);
+		} else {
+			ret = snd_soc_dai_set_tdm_slot(cpu_dai, 0, 0x03, slots,
+						       slot_width);
+		}
+
+		if (ret < 0) {
+			dev_err(rtd->dev,
+				"%s: failed to set tdm slot, err:%d\n",
+				__func__, ret);
+			goto end;
+		}
+
+		ret = snd_soc_dai_set_channel_map(cpu_dai, 0, NULL, channels,
+						  tdm_slot_offset);
+		if (ret < 0) {
+			dev_err(rtd->dev,
+				"%s: failed to set channel map, err:%d\n",
+				__func__, ret);
+			goto end;
+		}
+	} else {
+		ret = snd_soc_dai_set_tdm_slot(cpu_dai, 0xf, 0, slots,
+					       slot_width);
+		if (ret < 0) {
+			dev_err(rtd->dev,
+				"%s: failed to set tdm slot, err:%d\n",
+				__func__, ret);
+			goto end;
+		}
+
+		ret = snd_soc_dai_set_channel_map(cpu_dai, channels,
+						  tdm_slot_offset, 0, NULL);
+		if (ret < 0) {
+			dev_err(rtd->dev,
+				"%s: failed to set channel map, err:%d\n",
+				__func__, ret);
+			goto end;
+		}
+	}
+
+end:
+	return ret;
 }
 
 static int sm8250_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 				     struct snd_pcm_hw_params *params)
 {
-	struct snd_interval *rate = hw_param_interval(params,
-					SNDRV_PCM_HW_PARAM_RATE);
-	struct snd_interval *channels = hw_param_interval(params,
-					SNDRV_PCM_HW_PARAM_CHANNELS);
+	struct snd_interval *rate =
+		hw_param_interval(params, SNDRV_PCM_HW_PARAM_RATE);
+	struct snd_interval *channels =
+		hw_param_interval(params, SNDRV_PCM_HW_PARAM_CHANNELS);
 
 	rate->min = rate->max = 48000;
 	channels->min = channels->max = 2;
@@ -52,14 +141,25 @@ static int sm8250_snd_startup(struct snd_pcm_substream *substream)
 	unsigned int codec_dai_fmt = SND_SOC_DAIFMT_BC_FC;
 	struct snd_soc_pcm_runtime *rtd = snd_soc_substream_to_rtd(substream);
 	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
-	struct snd_soc_dai *codec_dai = snd_soc_rtd_to_codec(rtd, 0);
+	struct snd_soc_dai *codec_dai;
+	int ret, j;
+	bool is_aw88261 = false;
+
+	/* Check if we're dealing with an AW88261 codec */
+	for_each_rtd_codec_dais(rtd, j, codec_dai) {
+		if (strstr(codec_dai->component->name, "aw88261") != NULL) {
+			is_aw88261 = true;
+			break;
+		}
+	}
 
 	switch (cpu_dai->id) {
 	case PRIMARY_MI2S_RX:
 		codec_dai_fmt |= SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_I2S;
 		snd_soc_dai_set_sysclk(cpu_dai,
-			Q6AFE_LPASS_CLK_ID_PRI_MI2S_IBIT,
-			MI2S_BCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
+				       Q6AFE_LPASS_CLK_ID_PRI_MI2S_IBIT,
+				       MI2S_BCLK_RATE,
+				       SNDRV_PCM_STREAM_PLAYBACK);
 		snd_soc_dai_set_fmt(cpu_dai, fmt);
 		snd_soc_dai_set_fmt(codec_dai, codec_dai_fmt);
 		break;
@@ -74,10 +174,88 @@ static int sm8250_snd_startup(struct snd_pcm_substream *substream)
 	case TERTIARY_MI2S_RX:
 		codec_dai_fmt |= SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_I2S;
 		snd_soc_dai_set_sysclk(cpu_dai,
-			Q6AFE_LPASS_CLK_ID_TER_MI2S_IBIT,
-			MI2S_BCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
+				       Q6AFE_LPASS_CLK_ID_TER_MI2S_IBIT,
+				       MI2S_BCLK_RATE,
+				       SNDRV_PCM_STREAM_PLAYBACK);
 		snd_soc_dai_set_fmt(cpu_dai, fmt);
 		snd_soc_dai_set_fmt(codec_dai, codec_dai_fmt);
+		break;
+	case TERTIARY_TDM_RX_0:
+		/* AW88261 specific format and clock setting (Xiaomi Pad 6 fix) */
+		if (is_aw88261) {
+			/* DSP_A mode needed for AW88261 */
+			codec_dai_fmt |= SND_SOC_DAIFMT_NB_NF |
+					 SND_SOC_DAIFMT_DSP_A;
+
+			/* AW88261 needs a specific clock rate for stable PLL operation */
+			ret = snd_soc_dai_set_sysclk(
+				cpu_dai, Q6AFE_LPASS_CLK_ID_TER_TDM_IBIT,
+				11289600, SNDRV_PCM_STREAM_PLAYBACK);
+
+			if (ret < 0) {
+				dev_warn(
+					rtd->dev,
+					"Setting TDM clock failed: %d, trying standard rate\n",
+					ret);
+				ret = snd_soc_dai_set_sysclk(
+					cpu_dai,
+					Q6AFE_LPASS_CLK_ID_TER_TDM_IBIT,
+					TDM_BCLK_RATE,
+					SNDRV_PCM_STREAM_PLAYBACK);
+				if (ret < 0) {
+					dev_err(rtd->dev,
+						"Failed to set any TDM clock rate: %d\n",
+						ret);
+					return ret;
+				}
+			} else {
+				dev_info(
+					rtd->card->dev,
+					"Setting AW88261 special clock: 11.2896MHz\n");
+			}
+		} else {
+			codec_dai_fmt |= SND_SOC_DAIFMT_NB_NF |
+					 SND_SOC_DAIFMT_DSP_A;
+			ret = snd_soc_dai_set_sysclk(
+				cpu_dai, Q6AFE_LPASS_CLK_ID_TER_TDM_IBIT,
+				TDM_BCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
+			if (ret < 0) {
+				dev_err(rtd->dev,
+					"Failed to set TDM clock rate: %d\n",
+					ret);
+				return ret;
+			}
+		}
+
+		/* Set format for all codec DAIs */
+		for_each_rtd_codec_dais(rtd, j, codec_dai) {
+			ret = snd_soc_dai_set_fmt(codec_dai, codec_dai_fmt);
+			if (ret < 0) {
+				dev_err(rtd->dev,
+					"TDM fmt err:%d for codec %s\n", ret,
+					codec_dai->component->name);
+				continue; /* Continue with other codecs even if one fails */
+			}
+
+			if (is_aw88261) {
+				/* Try to set the codec clock to match our TDM clock */
+				ret = snd_soc_dai_set_sysclk(
+					codec_dai, 0, 11289600,
+					SNDRV_PCM_STREAM_PLAYBACK);
+				if (ret < 0) {
+					dev_dbg(rtd->dev,
+						"Setting codec sysclk failed: %d, trying standard rate\n",
+						ret);
+					snd_soc_dai_set_sysclk(
+						codec_dai, 0, TDM_BCLK_RATE,
+						SNDRV_PCM_STREAM_PLAYBACK);
+				}
+			} else {
+				snd_soc_dai_set_sysclk(
+					codec_dai, 0, TDM_BCLK_RATE,
+					SNDRV_PCM_STREAM_PLAYBACK);
+			}
+		}
 		break;
 	default:
 		break;
@@ -104,7 +282,13 @@ static int sm8250_snd_hw_params(struct snd_pcm_substream *substream,
 	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
 	struct sm8250_snd_data *pdata = snd_soc_card_get_drvdata(rtd->card);
 
-	return qcom_snd_sdw_hw_params(substream, params, &pdata->sruntime[cpu_dai->id]);
+	switch (cpu_dai->id) {
+	case PRIMARY_TDM_RX_0 ... QUINARY_TDM_TX_7:
+		return sm8250_tdm_snd_hw_params(substream, params);
+	}
+
+	return qcom_snd_sdw_hw_params(substream, params,
+				      &pdata->sruntime[cpu_dai->id]);
 }
 
 static int sm8250_snd_prepare(struct snd_pcm_substream *substream)
@@ -158,6 +342,8 @@ static int sm8250_platform_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	int ret;
 
+	printk(KERN_INFO "snd8250_platform_probe test2xd\n");
+
 	card = devm_kzalloc(dev, sizeof(*card), GFP_KERNEL);
 	if (!card)
 		return -ENOMEM;
@@ -181,9 +367,9 @@ static int sm8250_platform_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id snd_sm8250_dt_match[] = {
-	{.compatible = "qcom,sm8250-sndcard"},
-	{.compatible = "qcom,qrb4210-rb2-sndcard"},
-	{.compatible = "qcom,qrb5165-rb5-sndcard"},
+	{ .compatible = "qcom,sm8250-sndcard" },
+	{ .compatible = "qcom,qrb4210-rb2-sndcard" },
+	{ .compatible = "qcom,qrb5165-rb5-sndcard" },
 	{}
 };
 
