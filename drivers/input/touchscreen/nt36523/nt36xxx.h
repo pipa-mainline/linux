9a690fdd6aee4a7b84e960d035ef209a794dc77c
vipoll <vipollmail@gmail.com>
2025-06-29 22:35:51 +0400

    nt36xxx: fix module loading

--- drivers/input/touchscreen/nt36523/nt36xxx.h
+++ drivers/input/touchscreen/nt36523/nt36xxx.h
@@ -59,7 +59,7 @@ struct nt36532 {
 
 
 //---SPI driver info.---
-#define NVT_SPI_NAME "NVT-ts"
+#define NVT_SPI_NAME "NVT-ts-spi"
 
 #if NVT_DEBUG
 #define NVT_LOG(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_SPI_NAME, __func__, __LINE__, ##args)
