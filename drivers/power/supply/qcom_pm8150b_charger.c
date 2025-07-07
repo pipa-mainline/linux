e9821f4f171c2166d701978cdb34f796c077ad11
Arseniy Velikanov <me@adomerle.pw>
2025-07-07 22:19:24 +0400

    pm8150b_charger: Fix compilation in linux-next
    Signed-off-by: Arseniy Velikanov <me@adomerle.pw>

--- drivers/power/supply/qcom_pm8150b_charger.c
+++ drivers/power/supply/qcom_pm8150b_charger.c
@@ -856,7 +856,7 @@ static int smb5_probe(struct platform_device *pdev)
 		return rc;
 
 	supply_config.drv_data = chip;
-	supply_config.of_node = pdev->dev.of_node;
+	supply_config.fwnode = dev_fwnode(&pdev->dev);
 
 	desc = devm_kzalloc(chip->dev, sizeof(smb5_psy_desc), GFP_KERNEL);
 	if (!desc)
