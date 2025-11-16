38a617d78ba5178ebc8c986109abeebd0b8f8da5
rmuxnet <116457149+rmuxnet@users.noreply.github.com>
2025-11-16 14:13:37 +0000

    drm/panel: panel-novatek-nt36532: Add 60Hz display mode

--- drivers/gpu/drm/panel/panel-novatek-nt36532.c
+++ drivers/gpu/drm/panel/panel-novatek-nt36532.c
@@ -324,6 +324,21 @@ static const struct drm_display_mode nt36532_mode_120 = {
 	.height_mm = 237,
 	.type = DRM_MODE_TYPE_DRIVER,
 };
+
+static const struct drm_display_mode nt36532_mode_60 = {
+	.clock = (1800 + 200 + 4 + 92) * (2880 + 26 + 2 + 214) * 60 / 1000,
+	.hdisplay = 1800,
+	.hsync_start = 1800 + 200,
+	.hsync_end = 1800 + 200 + 4,
+	.htotal = 1800 + 200 + 4 + 92,
+	.vdisplay = 2880,
+	.vsync_start = 2880 + 26,
+	.vsync_end = 2880 + 26 + 2,
+	.vtotal = 2880 + 26 + 2 + 214,
+	.width_mm = 148,
+	.height_mm = 237,
+	.type = DRM_MODE_TYPE_DRIVER,
+};
 static const struct panel_desc pipa_desc = {
 	.dsi_info = {
 		.type = "pipa",
@@ -477,9 +492,15 @@ static int nt36532_unprepare(struct drm_panel *panel)
 static int nt36532_get_modes(struct drm_panel *panel,
 					struct drm_connector *connector)
 {
-	const struct drm_display_mode *mode;
-	mode = &nt36532_mode_120;
-	return drm_connector_helper_get_modes_fixed(connector, mode);
+	const struct drm_display_mode *modes[] = {
+		&nt36532_mode_120,
+		&nt36532_mode_60,
+	};
+	int count = 0, i;
+
+	for (i = 0; i < ARRAY_SIZE(modes); i++)
+		count += drm_connector_helper_get_modes_fixed(connector, modes[i]);
+	return count;
 }
 
 static const struct drm_panel_funcs nt36532_panel_funcs = {
